6188781fc33957b5b8b3ca80e4630803b96daaab
TusharK <ktusharhk@gmail.com>
2020-06-08 17:03:05 +1000

    added app snap-shots

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,10 +1,11 @@
+// Function to devour the burger.
 $(function () {
     $(".btn-devour").on("click", function (event) {
         var id = $(this).data("id");
         // Send the PUT request.
         $.ajax("/api/burgers/" + id, {
             type: "PUT",
-        
+
         }).then(
             function () {
                 console.log("changed status to devoured in the controller");
@@ -14,25 +15,25 @@ $(function () {
         );
     });
 
-    // WIP
-    $(".create-form").on("submit", function(event) {
+    // Add new burger on submit
+    $(".create-form").on("submit", function (event) {
         // Make sure to preventDefault on a submit event.
         event.preventDefault();
-    
+
         var newBurger = {
-          name: $("#wish-burg").val().trim()
+            name: $("#wish-burg").val().trim()
         };
-    
+
         // Send the POST request.
         $.ajax("/api/burgers", {
-          type: "POST",
-          data: newBurger
+            type: "POST",
+            data: newBurger
         }).then(
-          function() {
-            console.log("Added new burger in wish list");
-            // Reload the page to get the updated list
-            location.reload();
-          }
+            function () {
+                console.log("Added new burger in wish list");
+                // Reload the page to get the updated list
+                location.reload();
+            }
         );
-      });
+    });
 });
